bb2906f5b728b367e0914fc4c4ed06ca9523516f
Squidamatron <claspysheep@gmail.com>
2014-10-16 21:42:45 -0400

    Force Steam windows to float Steam doesn't like being tiled too much, so floating is MUCH more preferable in this case.

--- config.h
+++ config.h
@@ -45,6 +45,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            True,        -1 },
+	{ "Steam",     NULL,       NULL,       0,            True,        -1 },
 	//{ "Firefox",  NULL,       NULL,       1 << 8,       False,       -1 },
 	{ "Firefox",  NULL,       NULL,       0,            False,       -1 },
 	{ "feh",      NULL,       NULL,       0,            True,        -1 },
